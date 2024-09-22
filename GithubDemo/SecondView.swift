b6945135b34710028bbaad2899ccb52b3bdde801
gurayyalmaz <guray.yl93@gmail.com>
2024-09-22 17:01:10 +0200

    Create SecondView

---
+++ GithubDemo/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GithubDemo
+//
+//  Created by Gyuray Yalmaz on 22.09.24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
