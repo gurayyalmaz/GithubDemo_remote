c46daaef622cf7137dc2958ec20fdcc65b8f3421
gurayyalmaz <guray.yl93@gmail.com>
2024-09-22 16:43:26 +0200

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Gyuray Yalmaz on 22.09.24.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
