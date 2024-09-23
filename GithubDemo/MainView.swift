1b27585f3b765834617d6d9410f07f6f6ffa3511
gurayyalmaz <guray.yl93@gmail.com>
2024-09-23 05:34:46 +0200

    Change background color to red

--- GithubDemo/MainView.swift
+++ GithubDemo/MainView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("This is a test")
+        
+        ZStack {
+            
+            Color.red.ignoresSafeArea(.all)
+            
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("This is a test")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
